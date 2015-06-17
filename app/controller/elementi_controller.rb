2a65edd855812abbb013ad9922d7e8453075b8c7
k1ra <darkslipknot@msn.com>
2015-06-17 17:19:41 +0200

    elemento_controller_view_create_due_file

---
+++ app/controller/elementi_controller.rb
@@ -0,0 +1,39 @@
+class ElementiController < ApplicationController
+
+def create
+  @ordine = current_ordine
+  @prodotto = elemento_params[:prodotto_id]
+  @taglia = elemento_params[:taglia]
+
+  elementi = @ordine.elementi.where(prodotto_id: @prodotto, taglia: @taglia)
+
+  if elementi.any?
+    elementi.each {|elemento| elemento.quantita += 1 }
+  else
+    @elemento = @ordine.elementi.new(elemento_params)
+  end
+
+  @ordine.save
+  session[:ordine_id] = @ordine.id
+end
+
+  def update
+    @ordine = current_ordine
+    @elemento = @ordine.elementi.find(params[:id])
+    @elemento.update_attributes(elemento_params)
+    @elementi = @ordine.elementi
+  end
+
+  def destroy
+    @ordine = current_ordine
+    @no_elementi = current_ordine_destroy
+    @elemento = @ordine.elementi.find(params[:id])
+    @elemento.destroy
+    @elementi = @ordine.elementi
+  end
+private
+
+  def elemento_params
+    params.require(:elemento).permit(:taglia, :quantita, :prodotto_id, :ordine_id)
+  end
+end
