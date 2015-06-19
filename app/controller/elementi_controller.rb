4ed58216138e6d0ccd5a6e91adb227e682d2d893
k1ra <darkslipknot@msn.com>
2015-06-19 17:49:22 +0200

    Update elementi_controller.rb

--- app/controller/elementi_controller.rb
+++ app/controller/elementi_controller.rb
@@ -5,10 +5,12 @@ def create
   @prodotto = elemento_params[:prodotto_id]
   @taglia = elemento_params[:taglia]
 
-  elementi = @ordine.elementi.where(prodotto_id: @prodotto, taglia: @taglia)
+  @elemento = @ordine.elementi.find_by(prodotto_id: @prodotto, taglia: @taglia)
 
-  if elementi.any?
-    elementi.each {|elemento| elemento.quantita += 1 }
+  if @elemento 
+    @ordine = current_ordine
+    @ordine.elementi.each {|elemento| elemento.quantita += 1 }
+    @elementi = @ordine.elementi
   else
     @elemento = @ordine.elementi.new(elemento_params)
   end
